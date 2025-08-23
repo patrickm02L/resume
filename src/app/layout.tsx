aff9b294525f3c0f034e39af3def476c94c0078a
Patrick McDonnell <patrickm02L@gmail.com>
2025-08-23 14:32:54 -0500

    Add preload hint for profile image to fix first-load rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/resume/patrick-profile.png" as="image" />
+      </head>
       <body
         className={`${inter.variable} antialiased font-inter`}
       >
